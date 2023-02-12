3d190aed7e5f39489aca507cd88dbe132f798888
serrarius <123832543+serrarius@users.noreply.github.com>
2023-02-12 13:52:57 +0300

    Update risotto_helper.py

--- risotto_helper.py
+++ risotto_helper.py
@@ -8,15 +8,11 @@ def isfloat(value):
 while True:
     # The program prompts user to enter either the number of risotto servings or the weight of the rice with which we want to cook the risotto.
     # It's assumed that the user knows how to cook risotto.
-    # The value is assigned to the variable num0.
-    # It is assumed that the portions should not be more than 12, as in the home to prepare more portions at once will be difficult.
-    # In turn, the rice should not be less than 50 grams.
-    # With less rice we are unlikely to make a sufficient amount of risotto.
+    # Also it's assumed that the portions should not be more than 12, and the rice should not be less than 50 grams.
     print('Введите в цифровом значении кол-во порций ризотто (не более 12) или вес риса в граммах (не менее 50): ', end='')    
     num0 = input()
-    # Since in Russia the decimal separator is a comma, we need be able to replace the comma with a period,
-    # just in case, so that no error will occur due to entering a value with a comma.
-    # However, for aesthetic reasons, in the future the dot will have to be replaced by a comma, for which the replace function will be used.
+    # Since in Russia the decimal separator is a comma, we need be able to replace the comma with a period.
+    # The dot will be replaced back by a comma in the output.
     if ',' in num0:
         num0 = num0.replace(',', '.')
     # This starts checking if num0 is a positive number.
@@ -28,7 +24,6 @@ while True:
             print('\nДля приготовления ризотто из', str(num1).replace('.',','), 'г риса нужно взять:')        
             break
         # The cycle begins again if the user enters a number greater than 12 or less than 50.
-        # The user has the ability to re-enter the desired value without restarting the program.
         elif num0 > 12:
             print('Либо слишком много порций, либо слишком мало риса. Попробуйте еще раз.\n')   
         else:
@@ -36,7 +31,7 @@ while True:
             # We take 62.5 grams as the weight of the rice for one serving.
             num1 = int(62.5 * num0)
             # If num0 is a number with a decimal separator, we do not convert num0 to an integer.
-            # If num0 is an integer, however, we prefer this conversion just for aesthetic purposes.
+            # If num0 is an integer, however, we need this conversion.
             # The word "порция", meaning portion, is declined in the genitive case in this case.
             # But if num0 == 1, "portion" must be declined in the accusative case, so there is a separate case for that.
             if num0 == 1:
@@ -48,7 +43,6 @@ while True:
             print('риса —', num1, 'г;') 
             break           
     # The cycle starts again if user enter zero, a negative number or a value that is not a number.
-    # The user has the possibility to re-enter the desired value without restarting the program.
     else:
         print('Введенное значение не является числом, либо не может быть применено для расчета. Попробуйте еще раз.\n')
 # The program calculates and prints the amount of other ingredients based on the amount of rice in the specified proportions.

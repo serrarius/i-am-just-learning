2d3ae642a98c7507ffe85c34a7e5733d308ea564
serrarius <123832543+serrarius@users.noreply.github.com>
2023-02-14 13:22:37 +0300

    Update risotto_helper.py

--- risotto_helper.py
+++ risotto_helper.py
@@ -1,58 +1,69 @@
-# A function for checking if the entered value is an integer number or a floating point number.
-def isfloat(value):
+max_portions = 12
+min_rice = 50
+rice_in_portion = 62.5
+
+# Since in Russia the decimal separator is a comma, we need be able to replace the comma with a period to work with value.
+def comma_to_period(value):
+    return value.replace(',', '.')
+
+# For the same reasons, values containing a period will be output with a comma.
+def float_period(value):
+    return float(comma_to_period(value))
+
+def is_float(value):
     try:
         float(value)
         return True
     except ValueError:
-       return False
+        return False
+
+def int_or_comma(value):
+    if value % 1 == 0:
+        return int(value)
+    else:
+        return (str(value)).replace('.', ',')
+
+
+def portions_s_or_pl(value):
+    if value == 1:
+        return 'одной порции'
+    else:
+        return f'{int_or_comma(value)} порций'
+
 while True:
     # The program prompts user to enter either the number of risotto servings or the weight of the rice with which we want to cook the risotto.
     # It's assumed that the user knows how to cook risotto.
     # It's also assumed that the portions should not be more than 12, and the rice should not be less than 50 grams.
-    print('Введите в цифровом значении кол-во порций ризотто (не более 12) или вес риса в граммах (не менее 50): ', end='')    
-    num0 = input()
-    # Since in Russia the decimal separator is a comma, we need be able to replace the comma with a period.
-    # The period will be replaced back by a comma in the output.
-    if ',' in num0:
-        num0 = num0.replace(',', '.')
-    if isfloat(num0) and float(num0) > 0:
-        num0 = float(num0)
-        if num0 >= 50:
-            num1 = num0
-            if num0 % 1 == 0:
-                print('\nДля приготовления ризотто из', int(num0), 'г риса нужно взять:')
-            else:
-                print('\nДля приготовления ризотто из', str(num0).replace('.',','), 'г риса нужно взять:')        
-            break
-        # The cycle begins again if the user enters a number greater than 12 or less than 50.
-        elif num0 > 12:
-            print('Либо слишком много порций, либо слишком мало риса. Попробуйте еще раз.\n')   
-        else:
-            # Multiply the number of servings by the weight of the rice for one serving.
-            # We take 62.5 grams as the weight of the rice for one serving.
-            num1 = int(62.5 * num0)
-            # If num0 is a number with digits other than zero after the decimal separator, we do not convert num0 to an integer.
-            # If not, we need this conversion.
-            # The word "порция", meaning portion, is declined in the genitive case.
-            # If num0 == 1, the word is declined in the singular, otherwise it is declined in the plural.
-            if num0 == 1:
-                print('\nДля приготовления одной порции ризотто нужно взять:')
-            elif num0 % 1 == 0:
-                print('\nДля приготовления', int(num0), 'порций ризотто нужно взять:')
-            else:
-                print('\nДля приготовления', str(num0).replace('.',','), 'порций ризотто нужно взять:')
-            print('риса —', num1, 'г;') 
-            break           
-    # The cycle starts again if user enter zero, a negative number or a value that is not a number.
+    print('Введите в цифровом формате кол-во порций ризотто (не более 12) или вес риса в граммах (не менее 50): ', end='')
+    entered_value = comma_to_period(input())
+    if is_float(entered_value) == False:
+        print('Введенное значение не является числом или введено в некорректном формате. Попробуйте еще раз.\n')
+        continue
+    entered_value = float(entered_value)
+    if 0 >= entered_value:
+        print('Введенное значение не может быть применено для расчета. Попробуйте еще раз.\n')
+        continue
+    elif max_portions < entered_value < min_rice:
+        print('Либо слишком много порций, либо слишком мало риса. Попробуйте еще раз.\n')
+        continue
+    elif 0 < entered_value <= max_portions:
+        amount_of_rice = entered_value * rice_in_portion
+        print(f'''\nДля приготовления {portions_s_or_pl(entered_value)} ризотто нужно взять:
+риса — {int(amount_of_rice)} г;''')
+        break
     else:
-        print('Введенное значение не является числом, либо не может быть применено для расчета. Попробуйте еще раз.\n')
+        print(f'''Для приготовления ризотто из {int_or_comma(entered_value)} г риса нужно взять:''')
+        amount_of_rice = int(entered_value)
+        break
+    
+    
 # The program calculates and prints the amount of other ingredients based on the amount of rice in the specified proportions.
-print('сливочного масла —', int(num1 * 0.3), end=' г, ')
-print('из которого взять для жарки', int(num1 * 0.3 / 3), 'г', end=' ')
-print('и оставить на конец', int(num1 * 0.3 / 3 * 2), 'г;')
-print('сыра —', int(num1 * 0.24), 'г;')
-print('лука —', int(num1 * 0.2), 'г;')
-print('бульона —', int(num1 * 5), 'мл;')
-print('вина —', int(num1 * 0.38), 'мл.\n')
+print(f'''сливочного масла — {int(amount_of_rice * 0.3)} г,''',
+f'''из которого взять для жарки {int(amount_of_rice * 0.3 / 3)} г''',
+f'''и оставить на конец {int(amount_of_rice * 0.3 / 3 * 2)} г,
+сыра — {int(amount_of_rice * 0.24)} г;
+лука — {int(amount_of_rice * 0.2)} г;
+бульона — {int(amount_of_rice * 5)} мл;
+вина — {int(amount_of_rice * 0.38)} мл.''')
 import os
-os.system('pause') 
+os.system('pause')
